96a42aca0a696aecf8e1fd1c754c59d8163cdcac
Piet <piet.verwilligen@cern.ch>
2014-11-13 11:22:50 +0100

    debug/add printout

--- RecoLocalMuon/GEMRecHit/test/testGEMCSCsegementanalyzer_cfg.py
+++ RecoLocalMuon/GEMRecHit/test/testGEMCSCsegementanalyzer_cfg.py
@@ -6,9 +6,9 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
 process.load('Configuration.Geometry.GeometryExtended2019_cff')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load("Geometry.GEMGeometry.gemGeometry_cfi")
-process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
-process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+# process.load("Geometry.GEMGeometry.gemGeometry_cfi")
+# process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
+# process.load("Geometry.CSCGeometry.cscGeometry_cfi")
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
@@ -18,13 +18,15 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:/lustre/cms/store/user/radogna/GEMCSCSegment/FullDigi_withNewSSegm/out_rec_gemcsc.test10.root'
+        # 'file:/lustre/cms/store/user/radogna/GEMCSCSegment/FullDigi_withNewSSegm/out_rec_gemcsc.test10.root'
+        # 'file:out_reco10_paranormal.root'
+        'file:out_rec_gemcsc.test10_xcommit.root'
     )
 )
 
 process.gemcscs = cms.EDAnalyzer('TestGEMCSCSegmentAnalyzer',
                               RootFileName = cms.untracked.string("TestGEMCSCSegmentHistograms_XdofFD10.root"),
-
+                              Debug = cms.untracked.bool(True),
 )
 
 process.p = cms.Path(process.gemcscs)
