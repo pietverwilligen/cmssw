7d738a5a2ea95106efe9b52f7e19ddc07902c5aa
Piet <piet.verwilligen@cern.ch>
2014-11-13 11:14:29 +0100

    changed dPhiChainBoxMax from 0.0 to 1.0 (healthy default)

--- RecoLocalMuon/GEMRecHit/python/gemcscSegments_cfi.py
+++ RecoLocalMuon/GEMRecHit/python/gemcscSegments_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 gemcscSegments = cms.EDProducer("GEMCSCSegmentProducer",
- #define input
+# define input
                                 inputObjectsGEM = cms.InputTag("gemRecHits","","RECO"),
                                 #inputObjectsGEM = cms.InputTag("gemRecHits","","NEWRECO"),
-                                inputObjectsCSC = cms.InputTag("cscSegments","","RECO"),#2 input gem rec hits e csc segment
-                                #inputObjectsCSC = cms.InputTag("cscSegments","","NEWRECO"),#2 input gem rec hits e csc segment
+                                inputObjectsCSC = cms.InputTag("cscSegments","","RECO"),    #2 input gem rec hits and csc segments
+                                #inputObjectsCSC = cms.InputTag("cscSegments","","NEWRECO"),#2 input gem rec hits and csc segments
  algo_name = cms.string("GEMCSCSegAlgoRR"),                             
  algo_psets = cms.PSet(
      GEMCSCDebug = cms.untracked.bool(True),
@@ -14,16 +14,16 @@ gemcscSegments = cms.EDProducer("GEMCSCSegmentProducer",
      dXclusBoxMax = cms.double(1.),
      dYclusBoxMax = cms.double(5.),
      preClusteringUseChaining = cms.bool(True),
-                       #dPhiChainBoxMax = cms.double(1.0),
-                       #dPhiChainBoxMax = cms.double(0.02),
-                       #dPhiChainBoxMax = cms.double(0.01),
-                       #dPhiChainBoxMax = cms.double(0.005),
-                       #dPhiChainBoxMax = cms.double(0.0025),
-                       #dPhiChainBoxMax = cms.double(0.001),
-                       dPhiChainBoxMax = cms.double(0.),
+                       dPhiChainBoxMax = cms.double(1.0),
+                       # dPhiChainBoxMax = cms.double(0.02),
+                       # dPhiChainBoxMax = cms.double(0.01),
+                       # dPhiChainBoxMax = cms.double(0.005),
+                       # dPhiChainBoxMax = cms.double(0.0025),
+                       # dPhiChainBoxMax = cms.double(0.001),
+                       # dPhiChainBoxMax = cms.double(0.),
+     # dThetaChainBoxMax = cms.double(1.),
      dThetaChainBoxMax = cms.double(0.02),
      dRChainBoxMax = cms.double(0.5),
-     #dThEtaChainBoxMax = cms.double(1.),
      maxRecHitsInCluster = cms.int32(6)
      )
 )
