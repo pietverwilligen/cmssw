8f6206ef5c4639065b3009f6d06b1b23fc587b6e
Piet <piet.verwilligen@cern.ch>
2014-11-14 12:45:37 +0100

    improve geometry analyer

--- Geometry/RPCGeometry/test/rpcgeo.py
+++ Geometry/RPCGeometry/test/rpcgeo.py
@@ -2,27 +2,37 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("Demo")
 # process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
-# process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
-# process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
-
+# process.load("Configuration.Geometry.GeometryExtended2015Reco_cff")
 process.load("Configuration.Geometry.GeometryExtended2023_cff")
-process.load("Configuration.Geometry.GeometryExtended2023Reco_cff")
-# process.load("Configuration.Geometry.GeometryExtended2023RPCUpscope_2p4_192_cff")
-# process.load('Configuration.Geometry.GeometryExtended2023RPCUpscopeReco_2p4_192_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#process.load('Geometry.CommonDetUnit.globalTrackingGeometry_cfi')
-#process.load('Geometry.MuonNumbering.muonNumberingInitialization_cfi')
+# process.load("Configuration.Geometry.GeometryExtended2015Reco_RPC2Gap_cff")
+# process.load("Configuration.Geometry.GeometryExtended2015Reco_RPC4RE11_cff")
 
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
 
+process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 process.source = cms.Source("EmptySource")
 
-process.MessageLogger = cms.Service("MessageLogger")
+process.MessageLogger = cms.Service("MessageLogger",
+    # --- get more debug printout ---
+    # activate LogDebug messages
+    # more info: https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideMessageLoggerDebug
+    destinations = cms.untracked.vstring(
+        'detailedInfo',
+         'critical'
+    ),
+    detailedInfo = cms.untracked.PSet(
+        threshold = cms.untracked.string('DEBUG')
+    ),
+    debugModules = cms.untracked.vstring(
+        'RPCGeometryESModule',
+        'RPCGeometryBuilderFromDDD',
+        'MuonNumberingInitialization' 
+    )
+)
 
 process.demo = cms.EDAnalyzer("RPCGEO")
 
