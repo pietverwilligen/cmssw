798610721194406a96c781b6613a5e9ebf33ee8d
Piet <piet.verwilligen@cern.ch>
2015-05-14 12:12:26 +0200

    cleaned up for PR

--- RecoLocalMuon/GEMCSCSegment/plugins/GEMCSCSegmentBuilder.h
+++ RecoLocalMuon/GEMCSCSegment/plugins/GEMCSCSegmentBuilder.h
@@ -34,17 +34,15 @@ class CSCStationIndex{
  public:
  CSCStationIndex():_region(0),_station(0),_ring(0),_chamber(0),_layer(0){};
  CSCStationIndex(int region, int station, int ring, int chamber, int layer):
-  _region(region),
-  _station(station),
-  _ring(ring),
-  _chamber(chamber),
-  _layer(layer){}
+  _region(region), _station(station), _ring(ring),_chamber(chamber),_layer(layer){}
   ~CSCStationIndex(){}
-  int region() const {return _region;}
+
+  int region()  const {return _region;}
   int station() const {return _station;}
-  int ring() const {return _ring;}
+  int ring()    const {return _ring;}
   int chamber() const {return _chamber;}
-  int layer() const {return _layer;}
+  int layer()   const {return _layer;}
+
   bool operator<(const CSCStationIndex& cscind) const{
     if(cscind.region()!=this->region())
       return cscind.region()<this->region();
@@ -58,6 +56,7 @@ class CSCStationIndex{
       return cscind.layer()<this->layer();
     return false;
   }
+  
  private:
   int _region;
   int _station;
