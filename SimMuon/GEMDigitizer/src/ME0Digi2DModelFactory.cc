b002cb1e3689dfd842b58a63c4e9f3dc4c893584
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-05 18:12:02 +0200

    implementing new digitizer for ME0 with pads

---
+++ SimMuon/GEMDigitizer/src/ME0Digi2DModelFactory.cc
@@ -0,0 +1,4 @@
+#include "FWCore/PluginManager/interface/PluginFactory.h"
+
+#include "SimMuon/GEMDigitizer/interface/ME0Digi2DModelFactory.h"
+EDM_REGISTER_PLUGINFACTORY(ME0Digi2DModelFactory, "ME0Digi2DModelFactory");
