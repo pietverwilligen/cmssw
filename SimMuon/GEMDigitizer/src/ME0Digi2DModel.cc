b30808a2887bf53b4572fd3fc2179cf6c52b2a49
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-10 17:47:54 +0200

    print debug

--- SimMuon/GEMDigitizer/src/ME0Digi2DModel.cc
+++ SimMuon/GEMDigitizer/src/ME0Digi2DModel.cc
@@ -7,18 +7,21 @@ ME0Digi2DModel::fillDigis(int rollDetId, ME0Digi2DCollection& digis)
   for (auto d: digi_)
   {
     digis.insertDigi(ME0DetId(rollDetId), d);
-
-    std::cout<<"[ME0Digi2DModel] DetId "<<ME0DetId(rollDetId)<<" set of Digis inserted: "<<d<<std::endl;
-    // does not work
-    /*
-    for(auto& digi:d)
-      {
-	std::cout<<"[ME0Digi2DModel] DetId "<<ME0DetId(rollDetId)<<" ME02DDigi inserted: "<<digi<<std::endl;
-      }
-    */
-
-
+    std::cout<<"[ME0Digi2DModel] DetId "<<ME0DetId(rollDetId)<<" Digi inserted: "<<d<<std::endl;
   }
   digi_.clear();
+}
+
+void 
+ME0Digi2DModel::printDigis(ME0Digi2DCollection& digis)
+{
+  ME0Digi2DCollection::DigiRangeIterator me0dgIt;
+  for (me0dgIt = digis.begin(); me0dgIt != digis.end(); ++me0dgIt){
+    const ME0DetId& me0Id = (*me0dgIt).first;
+    const ME0Digi2DCollection::Range& digiRange = (*me0dgIt).second;
+    for (ME0Digi2DCollection::const_iterator digi = digiRange.first; digi != digiRange.second;digi++) {
+      std::cout<<"[ME0Digi2DModel] :: print Digi :: in DetId "<<me0Id<<" digi "<<*digi<<std::endl;
+    }
+  }
 }
 
