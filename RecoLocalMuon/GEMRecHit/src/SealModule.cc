6657efb8dc1b5c80684f491634e4ffff0f1519de
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-23 12:05:53 +0200

    ME0 Clusterizer --- v0 --- compiled, but not tested

--- RecoLocalMuon/GEMRecHit/src/SealModule.cc
+++ RecoLocalMuon/GEMRecHit/src/SealModule.cc
@@ -3,6 +3,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoLocalMuon/GEMRecHit/interface/GEMRecHitAlgoFactory.h"
 #include "RecoLocalMuon/GEMRecHit/interface/ME0RecHitAlgoFactory.h"
+#include "RecoLocalMuon/GEMRecHit/interface/ME0RecHit2DAlgoFactory.h"
 
 #include "RecoLocalMuon/GEMRecHit/src/GEMRecHitProducer.h"
 DEFINE_FWK_MODULE(GEMRecHitProducer);
@@ -17,9 +18,16 @@ DEFINE_FWK_MODULE(ME0RecHitProducer);
 #include "RecoLocalMuon/GEMRecHit/src/ME0RecHitStandardAlgo.h"
 DEFINE_EDM_PLUGIN (ME0RecHitAlgoFactory, ME0RecHitStandardAlgo, "ME0RecHitStandardAlgo");
 
+
+#include "RecoLocalMuon/GEMRecHit/src/ME0RecHit2DProducer.h"
+DEFINE_FWK_MODULE(ME0RecHit2DProducer);
+
+#include "RecoLocalMuon/GEMRecHit/src/ME0RecHit2DStandardAlgo.h"
+DEFINE_EDM_PLUGIN (ME0RecHit2DAlgoFactory, ME0RecHit2DStandardAlgo, "ME0RecHit2DStandardAlgo");
+
+
 #include <RecoLocalMuon/GEMRecHit/src/ME0SegmentProducer.h>
 #include <RecoLocalMuon/GEMRecHit/src/ME0SegmentBuilderPluginFactory.h>
-
 #include <RecoLocalMuon/GEMRecHit/src/ME0SegAlgoMM.h>
 DEFINE_FWK_MODULE(ME0SegmentProducer);
 DEFINE_EDM_PLUGIN(ME0SegmentBuilderPluginFactory, ME0SegAlgoMM, "ME0SegAlgoMM");
