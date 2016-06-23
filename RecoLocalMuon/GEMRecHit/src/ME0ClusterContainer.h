f5bdbd044c9a0728a3e30fa8fef0acd4298f820e
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-23 12:20:35 +0200

    ME0 Clusterizer --- v0 --- compiled, but not tested

---
+++ RecoLocalMuon/GEMRecHit/src/ME0ClusterContainer.h
@@ -0,0 +1,6 @@
+#ifndef RecoLocalMuon_ME0ClusterContainer_h
+#define RecoLocalMuon_ME0ClusterContainer_h
+#include <set>
+class ME0Cluster;
+typedef std::set<ME0Cluster> ME0ClusterContainer;
+#endif
