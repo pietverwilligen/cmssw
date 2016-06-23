8561d0f05531132271f04f9b132395b3af8c6b2a
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-23 12:06:52 +0200

    adapt for strips

--- Geometry/GEMGeometry/interface/ME0EtaPartition.h
+++ Geometry/GEMGeometry/interface/ME0EtaPartition.h
@@ -44,6 +44,8 @@ public:
   /// that has a value range of [0., nstrip]
   LocalPoint  centreOfStrip(float strip) const;
   LocalError  localError(float strip) const;
+  LocalError  localError(float strip, float cluster_size) const;
+
 
   /// returns fractional strip number [0..nstrips] for a LocalPoint
   /// E.g., if local point hit strip #2, the fractional strip number would be
