6ebb42290b7cce1c625fbe4531c2245d0d195e08
piet verwilligen <piet.verwilligen@cern.ch>
2016-06-23 12:07:41 +0200

    update

--- SimMuon/GEMDigitizer/src/ME0Digi2DModel.cc
+++ SimMuon/GEMDigitizer/src/ME0Digi2DModel.cc
@@ -7,7 +7,7 @@ ME0Digi2DModel::fillDigis(int rollDetId, ME0Digi2DCollection& digis)
   for (auto d: digi_)
   {
     digis.insertDigi(ME0DetId(rollDetId), d);
-    std::cout<<"[ME0Digi2DModel] DetId "<<ME0DetId(rollDetId)<<" Digi inserted: "<<d<<std::endl;
+    // std::cout<<"[ME0Digi2DModel] DetId "<<ME0DetId(rollDetId)<<" Digi inserted: "<<d<<std::endl;
   }
   digi_.clear();
 }
