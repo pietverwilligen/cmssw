c619ae2e61ee01ea66ea55f412591b668a7445b4
piet verwilligen <piet.verwilligen@cern.ch>
2016-09-26 17:43:33 +0200

    update GT and geometry for 81X

--- SimMuon/GEMDigitizer/test/runGEMDigiReader_cfg.py
+++ SimMuon/GEMDigitizer/test/runGEMDigiReader_cfg.py
@@ -6,8 +6,10 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2019_cff')
+# process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
+# process.load('Configuration.Geometry.GeometryExtended2019_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
@@ -15,18 +17,19 @@ process.load('Configuration.StandardSequences.Digi_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:out_digi.root'
+        # 'file:out_digi.root'
+        'file:/lustre/home/piet/cmshome/SLC6/ME0_Studies/810_ME0Granularity/CMSSW_8_1_0_pre11/src/MyCmsDriverCommands/TDR_baseline/step2.root'
     )
 )
 
-process.dumper = cms.EDAnalyzer("GEMDigiReader"
+process.dumper = cms.EDAnalyzer("GEMDigiReader",
     simhitToken = cms.InputTag("g4SimHits","MuonGEMHits"), 
     gemDigiToken = cms.InputTag("simMuonGEMDigis"), 
     gemDigiSimLinkToken = cms.InputTag("simMuonGEMDigis","GEM") 
