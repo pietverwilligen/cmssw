88de938baf341042283183e2808c497cca028c15
piet verwilligen <piet.verwilligen@cern.ch>
2016-09-26 17:44:16 +0200

    update

--- SimMuon/GEMDigitizer/test/runME0DigiReader_cfg.py
+++ SimMuon/GEMDigitizer/test/runME0DigiReader_cfg.py
@@ -6,8 +6,10 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-process.load('Configuration.Geometry.GeometryExtended2023HGCalMuonReco_cff')
-process.load('Configuration.Geometry.GeometryExtended2023HGCalMuon_cff')
+# process.load('Configuration.Geometry.GeometryExtended2023D1Reco_cff')
+# process.load('Configuration.Geometry.GeometryExtended2023D1_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1ME0DevReco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1ME0Dev_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.SimIdeal_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
@@ -21,12 +23,13 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'file:/lustre/cms/store/user/rosma/SingleMuPlusPt30_14TeV_GEN-SIM_2023HGCalME0Geomv2_AllEtaPhi/crab_SingleMuPt30_FullPt_PU0_NoBkg_8etaPart_768Strip_1ns_RECO_RealRO/160705_110743/0000/out_reco_me0_rechits_ME0Muon_PU0_1.root'
+        # 'file:/lustre/cms/store/user/rosma/SingleMuPlusPt30_14TeV_GEN-SIM_2023HGCalME0Geomv2_AllEtaPhi/crab_SingleMuPt30_FullPt_PU0_NoBkg_8etaPart_768Strip_1ns_RECO_RealRO/160705_110743/0000/out_reco_me0_rechits_ME0Muon_PU0_1.root'
     )
 )
 
 process.dumper = cms.EDAnalyzer("ME0DigiReader",
-                                label = cms.untracked.string("simMuonME0Digis")
+    simhitToken = cms.InputTag("g4SimHits","MuonME0Hits"),
+    me0DigiToken = cms.InputTag("simMuonME0Digis")
 )
 
 process.p = cms.Path(process.dumper)
