30bc7f762f75a33a63be23d57fb30412ce9d85d6
piet verwilligen <piet.verwilligen@cern.ch>
2016-10-05 20:01:39 +0200

    extend ME0Chamber with previous functionalities to let ME0Geometry with 1 EtaPartition coexist

--- Geometry/GEMGeometry/interface/ME0Chamber.h
+++ Geometry/GEMGeometry/interface/ME0Chamber.h
@@ -5,6 +5,7 @@
 #include "DataFormats/MuonDetId/interface/ME0DetId.h"
 
 class ME0Layer;
+class ME0EtaPartition;
 
 class ME0Chamber : public GeomDet {
 public:
@@ -43,12 +44,33 @@ public:
   /// Retunr numbers of layers
   int nLayers() const;
 
+
+  /// To support the old ME0 Geometry (with 1 eta partition)
+  /// Add EtaPartition to the chamber which takes ownership 
+  void add(ME0EtaPartition* roll);
+
+  /// To support the old ME0 Geometry (with 1 eta partition)
+  /// Return the eta partition corresponding to the given id
+  const ME0EtaPartition* etaPartition(ME0DetId id) const;
+
+  const ME0EtaPartition* etaPartition(int isl) const;
+
+  /// To support the old ME0 Geometry (with 1 eta partition)
+  /// Return the eta partitions
+  const std::vector<const ME0EtaPartition*>& etaPartitions() const;
+
+  /// To support the old ME0 Geometry (with 1 eta partition)
+  /// Retunr numbers of eta partitions
+  int nEtaPartitions() const;
+
+
 private:
 
   ME0DetId detId_;
 
   // vector of layers for a chamber
   std::vector<const ME0Layer*> layers_;
-
+  // vector of eta partitions for a chamber
+  std::vector<const ME0EtaPartition*> etaPartitions_;
 };
 #endif
