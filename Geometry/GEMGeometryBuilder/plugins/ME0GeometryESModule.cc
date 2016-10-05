bc354fd336fe5133f8afd8fd6f8ac85f004581a2
piet verwilligen <piet.verwilligen@cern.ch>
2016-10-06 01:19:44 +0200

    added missing include file, included some logdebug

--- Geometry/GEMGeometryBuilder/plugins/ME0GeometryESModule.cc
+++ Geometry/GEMGeometryBuilder/plugins/ME0GeometryESModule.cc
@@ -5,6 +5,7 @@
 
 #include "Geometry/GEMGeometryBuilder/plugins/ME0GeometryESModule.h"
 #include "Geometry/GEMGeometryBuilder/src/ME0GeometryBuilderFromDDD.h"
+#include "Geometry/GEMGeometryBuilder/src/ME0GeometryBuilderFromDDD10EtaPart.h"
 #include "Geometry/GEMGeometryBuilder/src/ME0GeometryBuilderFromCondDB.h"
 
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
@@ -38,7 +39,11 @@ ME0GeometryESModule::~ME0GeometryESModule(){}
 std::shared_ptr<ME0Geometry>
 ME0GeometryESModule::produce(const MuonGeometryRecord & record) 
 {
+
+  LogTrace("ME0GeometryESModule")<<"ME0GeometryESModule::produce with useDDD = "<<useDDD<<" and use10EtaPart = "<<use10EtaPart;
+
   if(useDDD && !use10EtaPart){
+    LogTrace("ME0GeometryESModule")<<"ME0GeometryESModule::produce :: ME0GeometryBuilderFromDDD builder";
     edm::ESTransientHandle<DDCompactView> cpv;
     record.getRecord<IdealGeometryRecord>().get(cpv);
     edm::ESHandle<MuonDDDConstants> mdc;
@@ -47,6 +52,7 @@ ME0GeometryESModule::produce(const MuonGeometryRecord & record)
     return std::shared_ptr<ME0Geometry>(builder.build(&(*cpv), *mdc));
   }
   else if(useDDD && use10EtaPart){
+    LogTrace("ME0GeometryESModule")<<"ME0GeometryESModule::produce :: ME0GeometryBuilderFromDDD10EtaPart builder";
     edm::ESTransientHandle<DDCompactView> cpv;
     record.getRecord<IdealGeometryRecord>().get(cpv);
     edm::ESHandle<MuonDDDConstants> mdc;
@@ -55,6 +61,7 @@ ME0GeometryESModule::produce(const MuonGeometryRecord & record)
     return std::shared_ptr<ME0Geometry>(builder.build(&(*cpv), *mdc));
   }
   else{
+    LogTrace("ME0GeometryESModule")<<"ME0GeometryESModule::produce :: ME0GeometryBuilderFromCondDB builder";
     edm::ESHandle<RecoIdealGeometry> rigme0;
     record.getRecord<ME0RecoGeometryRcd>().get(rigme0);
     ME0GeometryBuilderFromCondDB builder;
