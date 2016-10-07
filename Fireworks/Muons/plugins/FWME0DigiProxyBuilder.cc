aff718caf549c4d57a3f7e5c4b1cda12ebdc2a6c
piet verwilligen <piet.verwilligen@cern.ch>
2016-10-07 12:27:49 +0200

    update

--- Fireworks/Muons/plugins/FWME0DigiProxyBuilder.cc
+++ Fireworks/Muons/plugins/FWME0DigiProxyBuilder.cc
@@ -1,5 +1,7 @@
+
 //#include "TEveStraightLineSet.h"
-#include "TEveBoxSet.h"
+#include "TEveBox.h"
+#include "TEveLine.h"
 #include "TEveCompound.h"
 #include "TEveGeoNode.h"
 
@@ -28,50 +30,73 @@ private:
 
 void FWME0DigiProxyBuilder::build(const FWEventItem* iItem, TEveElementList* product, const FWViewContext*)
 {
-  const ME0DigiPreRecoCollection* digis = 0;
- 
-  iItem->get(digis);
+    const ME0DigiPreRecoCollection* digis = 0;
+    iItem->get(digis);
 
-  if ( ! digis ) 
-  {
-    fwLog(fwlog::kWarning)<<"Failed to get ME0Digis"<<std::endl;
-    return;
-  }
-  const FWGeometry *geom = iItem->getGeom();
-
-  for ( ME0DigiPreRecoCollection::DigiRangeIterator dri = digis->begin(), driEnd = digis->end();
-        dri != driEnd; ++dri )
-  {
-    unsigned int rawid = (*dri).first.rawId();
-    const ME0DigiPreRecoCollection::Range& range = (*dri).second;
-
-    if( ! geom->contains( rawid ))
+    if ( ! digis ) 
     {
-      fwLog( fwlog::kWarning ) << "Failed to get geometry of ME0 roll with detid: "
-                               << rawid << std::endl;
-      
-      TEveCompound* compound = createCompound();
-      setupAddElement( compound, product );
-      
-      continue;
+        std::cerr  << "Failed to get ME0Digis" <<std::endl;
+        return;
     }
-    
-    for( ME0DigiPreRecoCollection::const_iterator dit = range.first;
-	 dit != range.second; ++dit )
+    const FWGeometry *geom = iItem->getGeom();
+
+    int topCnt = 0;
+    for ( ME0DigiPreRecoCollection::DigiRangeIterator dri = digis->begin(), driEnd = digis->end();
+          dri != driEnd; ++dri )
     {
-      TEveBoxSet* stripDigiSet = new TEveBoxSet;
-      setupAddElement( stripDigiSet, product );
+        unsigned int rawid = (*dri).first.rawId();
+        const ME0DigiPreRecoCollection::Range& range = (*dri).second;
+
+        if( ! geom->contains( rawid ))
+        {
+            std::cerr << "Failed to get geometry of ME0 roll with detid: "
+                      << rawid << std::endl;
       
-      float localPoint[3] =    {(*dit).x(), (*dit).y(), 0.0};
-      float globalPoint[3];
+      
+            continue;
+        }
+          
+        int cntDS = 0;
+        for( ME0DigiPreRecoCollection::const_iterator dit = range.first;
+             dit != range.second; ++dit )
+        {
+            cntDS++;
+        }
+          
+        for( ME0DigiPreRecoCollection::const_iterator dit = range.first;
+             dit != range.second; ++dit )
+        {     
+            float localPoint[3] =    {(*dit).x(), (*dit).y(), 0.0};
+            float globalPoint[3];
+            geom->localToGlobal( rawid, localPoint, globalPoint);
+            
+            float x = globalPoint[0];
+            float y = globalPoint[1];
+            float z = globalPoint[2];
+            
+            float dx = (*dit).ex();
+            float dy = (*dit).ey();
+            float dz = 0.1;
+            
+            TEveBox* box = new TEveBox(); 
+            // points are set clockwise
+            box->SetVertex(0, x - dx, y + dy, z - dz );
+            box->SetVertex(1, x + dx, y + dy, z - dz );
+            box->SetVertex(2, x + dx, y - dy, z - dz );
+            box->SetVertex(3, x - dx, y - dy, z - dz );
 
-      geom->localToGlobal( rawid, localPoint, globalPoint);
+            box->SetVertex(4, x - dx, y + dy, z + dz );
+            box->SetVertex(5, x + dx, y + dy, z + dz );
+            box->SetVertex(6, x + dx, y - dy, z + dz );
+            box->SetVertex(7, x - dx, y - dy, z + dz );
+            
+            setupAddElement( box, product );
+        }
 
-      stripDigiSet->AddBox(globalPoint[0], globalPoint[1], globalPoint[2],
-                            (*dit).ex(), (*dit).ey(), 0.1);
+        topCnt++;          
     }
-  }
 }
+
 
 REGISTER_FWPROXYBUILDER(FWME0DigiProxyBuilder, ME0DigiPreRecoCollection, "ME0Digi", 
                         FWViewType::kAll3DBits | FWViewType::kAllRPZBits);
