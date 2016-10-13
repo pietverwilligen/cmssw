7aadb1014caecc03cbd1161ea9664d39737f1250
Marcello <marcello.maggi@cern.ch>
2016-10-13 11:55:22 +0200

    Importing CSC ST and RU Algo to ME0

---
+++ RecoLocalMuon/GEMSegment/python/me0SegmentsRU_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+RU_ME0 = cms.PSet(
+    doCollisions = cms.bool(True),
+    chi2Norm_2D_ = cms.double(35),
+    chi2_str = cms.double(50.0),
+    chi2Max = cms.double(100.0),
+    dPhiIntMax = cms.double(0.005),
+    dPhiMax = cms.double(0.006),
+    wideSeg = cms.double(3.0),
+    minLayersApart = cms.int32(1),
+    dRIntMax = cms.double(2.0),
+    dRMax = cms.double(1.5)
+)
+
+ME0SegAlgoRU = cms.PSet(
+    algo_name = cms.string('ME0SegAlgoRU'),
+    algo_psets = cms.VPSet(cms.PSet(RU_ME0))
+)
+
