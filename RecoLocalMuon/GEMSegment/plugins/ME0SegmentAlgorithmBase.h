bf8b9d0d8ec430fbacb8eab0c6976e8ce994c586
piet verwilligen <piet.verwilligen@cern.ch>
2016-12-20 17:55:23 +0100

    change refframe for RU seg algo to ME0Chamber

--- RecoLocalMuon/GEMSegment/plugins/ME0SegmentAlgorithmBase.h
+++ RecoLocalMuon/GEMSegment/plugins/ME0SegmentAlgorithmBase.h
@@ -15,6 +15,7 @@
 #include "DataFormats/GEMRecHit/interface/ME0RecHitCollection.h"
 #include "DataFormats/GEMRecHit/interface/ME0Segment.h"
 #include "Geometry/GEMGeometry/interface/ME0EtaPartition.h"
+#include "Geometry/GEMGeometry/interface/ME0Chamber.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include <map>
 #include <vector>
@@ -25,7 +26,8 @@ public:
   /// Typedefs
   typedef std::vector<const ME0RecHit*> EnsembleHitContainer;
   typedef std::vector<EnsembleHitContainer> ProtoSegments;
-  typedef std::pair<const ME0EtaPartition*, std::map<uint32_t, const ME0EtaPartition*> >ME0Ensemble;
+  // typedef std::pair<const ME0EtaPartition*, std::map<uint32_t, const ME0EtaPartition*> >ME0Ensemble;
+  typedef std::pair<const ME0Chamber*, std::map<uint32_t, const ME0EtaPartition*> >ME0Ensemble;
 
     /// Constructor
     explicit ME0SegmentAlgorithmBase(const edm::ParameterSet&) {};
