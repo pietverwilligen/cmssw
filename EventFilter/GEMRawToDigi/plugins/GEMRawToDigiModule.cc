c5b92c79e702120ad4048b1e21f637d68c400abf
yeckang <yechan.kang@cern.ch>
2021-10-27 13:20:11 +0200

    Update GEM eMap format

--- EventFilter/GEMRawToDigi/plugins/GEMRawToDigiModule.cc
+++ EventFilter/GEMRawToDigi/plugins/GEMRawToDigiModule.cc
@@ -79,7 +79,7 @@ void GEMRawToDigiModule::fillDescriptions(edm::ConfigurationDescriptions& descri
   desc.add<bool>("useDBEMap", false);
   desc.add<bool>("keepDAQStatus", false);
   desc.add<bool>("readMultiBX", false);
-  desc.add<unsigned int>("fedIdStart", FEDNumbering::MINGEMFEDID);
+  desc.add<unsigned int>("fedIdStart", FEDNumbering::MAXGEMFEDID);
   desc.add<unsigned int>("fedIdEnd", FEDNumbering::MAXGEMFEDID);
   descriptions.add("muonGEMDigisDefault", desc);
 }
@@ -149,7 +149,7 @@ void GEMRawToDigiModule::produce(edm::StreamID iID, edm::Event& iEvent, edm::Eve
         if (keepDAQStatus_) {
           outAMCStatus.get()->insertDigi(fedId, st_amc);
         }
-        continue;
+        //continue;
       }
 
       uint16_t amcBx = amc.bunchCrossing();
@@ -182,16 +182,14 @@ void GEMRawToDigiModule::produce(edm::StreamID iID, edm::Event& iEvent, edm::Eve
           // set vfat fw version
           vfat.setVersion(geb_dc.vfatVer);
           uint16_t vfatId = vfat.vfatId();
-          GEMROMapping::vfatEC vfat_ec{vfatId, gemChId};
+          GEMROMapping::vfatDC vfat_dc{geb_dc.chamberType, vfatId};
 
-          if (!gemROMap->isValidChipID(vfat_ec)) {
+          if (!gemROMap->isValidChipID(vfat_dc)) {
             st_oh.inValidVFAT();
             continue;
           }
 
-          GEMROMapping::vfatDC vfat_dc = gemROMap->vfatPos(vfat_ec);
-          vfat.setPhi(vfat_dc.localPhi);
-          GEMDetId gemId = vfat_dc.detId;
+          GEMDetId gemId(gemChId.region(), gemChId.ring(), gemChId.station(), gemChId.layer(), gemChId.chamber(), 0);
 
           GEMVFATStatus st_vfat(amc, vfat, vfat.phi(), readMultiBX_);
           if (st_vfat.isBad()) {
@@ -215,16 +213,23 @@ void GEMRawToDigiModule::produce(edm::StreamID iID, edm::Event& iEvent, edm::Eve
             if (chan0xf == 0)
               continue;
 
-            GEMROMapping::channelNum chMap{vfat_dc.vfatType, chan};
+            GEMROMapping::channelNum chMap{vfat_dc.chamberType, vfat_dc.vfatAdd, chan};
             GEMROMapping::stripNum stMap = gemROMap->hitPos(chMap);
 
-            int stripId = stMap.stNum + vfat.phi() * GEMeMap::maxChan_;
+            int stripId = stMap.stNum;
+            int ieta = stMap.iEta;
+
+            // ieta == 0 means not connected channel
+            if (ieta == 0) 
+              continue;
+
+            GEMDetId gemId(gemChId.region(), gemChId.ring(), gemChId.station(), gemChId.layer(), gemChId.chamber(), ieta);
 
             GEMDigi digi(stripId, bx);
 
             LogDebug("GEMRawToDigiModule")
                 << "fed: " << fedId << " amc:" << int(amcNum) << " geb:" << int(gebId) << " vfat id:" << int(vfatId)
-                << ",type:" << vfat_dc.vfatType << " id:" << gemId << " ch:" << chMap.chNum << " st:" << digi.strip()
+                << ",type:" << vfat_dc.chamberType << " id:" << gemId << " ch:" << chMap.chNum << " st:" << digi.strip()
                 << " bx:" << digi.bx();
 
             outGEMDigis.get()->insertDigi(gemId, digi);
